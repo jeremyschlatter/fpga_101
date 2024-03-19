26fb610720156d252235b01081f31ecc9954f10c
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2024-03-18 23:07:29 -0700

    completed step 5

--- lab001/base.py
+++ lab001/base.py
@@ -17,6 +17,10 @@ _io = [
     ("clk100", 0, Pins("E3"), IOStandard("LVCMOS33")),
 
     ("cpu_reset", 0, Pins("C12"), IOStandard("LVCMOS33")),
+
+    ("rgb_r", 0, Pins("N16"), IOStandard("LVCMOS33")),
+    ("rgb_g", 0, Pins("R11"), IOStandard("LVCMOS33")),
+    ("rgb_b", 0, Pins("G14"), IOStandard("LVCMOS33")),
 ]
 
 # Platform -----------------------------------------------------------------------------------------
@@ -33,16 +37,24 @@ class Platform(XilinxPlatform):
 # Create our platform (fpga interface)
 platform = Platform()
 led = platform.request("user_led")
+rgb_r = platform.request("rgb_r")
+rgb_g = platform.request("rgb_g")
+rgb_b = platform.request("rgb_b")
 
 # Create our module (fpga description)
 module = Module()
 
 # Create a counter and blink a led
 counter = Signal(27)
-seconds = Signal(3)
-module.comb += led.eq(seconds[0])
-module.sync += If(counter == 100_000_000,
-                  seconds.eq(seconds + 1),
+quarter_seconds = Signal(3)
+module.comb += [
+    led.eq(quarter_seconds[2]),
+    rgb_r.eq(quarter_seconds[2]),
+    rgb_g.eq(quarter_seconds[1]),
+    rgb_b.eq(quarter_seconds[0]),
+]
+module.sync += If(counter == 25_000_000,
+                  quarter_seconds.eq(quarter_seconds + 1),
                   counter.eq(0),
                   ).Else(counter.eq(counter + 1))
 
